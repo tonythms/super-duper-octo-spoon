651fe3f2b545a0198c166d839e5207502e322416
tonythms <tony.thomas@da-cons.de>
2017-05-08 12:17:30 +0200

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,9 +1,8 @@
-# Python Program to calculate the square root
-
-# Note: change this value for a different result
-num = 8 
-
-# uncomment to take the input from the user
-#num = float(input('Enter a number: '))
-num_sqrt = num ** 0.5
-print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+# This program adds two numbers
+"""its fun """
+N1 = 180
+N2 = 147
+# Add two numbers
+S = N1+N2
+# Display the sum
+print 'The sum of {0} and {1} is {2}' .format(N1, N2, S)
